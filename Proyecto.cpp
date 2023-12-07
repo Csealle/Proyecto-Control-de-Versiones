527fe85c203f4314e21146d845095cedca303ec8
Csealle <Allesca@proton.me>
2023-12-06 19:38:23 -0600

    Tomando Forma

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -12,41 +12,56 @@ int main() {
         cout << "\n*** Bienvenido al Espacio ***\n";
         cout << "\n--- Menú Principal ---\n";
         cout << "1. Explorar la galaxia desconocida\n";
-        cout << "2. Investigar un planeta alienígena\n";
-        cout << "3. Enfrentar una nave enemiga\n";
-        cout << "4. Descansar en la estación espacial\n";
-        cout << "5. Terminar la aventura\n";
+        cout << "2. Terminar la aventura\n";
         cout << "Tu elección: ";
         cin >> opcionUsuario;
 
         switch (opcionUsuario) {
             case 1:
-                cout << "Te adentras en un remolino estelar y encuentras una fuente de energía pura.\n";
-                cout << "Tu energía aumenta en 20 puntos.\n";
-                energia += 20;
+                cout << "\n--- Explorar la Galaxia Desconocida ---\n";
+                cout << "1. Investigar un campo de asteroides\n";
+                cout << "2. Investigar un agujero de gusano\n";
+                cout << "3. Explorar un planeta distante\n";
+                cout << "4. Escanear una nave abandonada\n";
+                cout << "5. Volver al menú principal\n";
+                cout << "Tu elección: ";
+                cin >> opcionUsuario;
+
+                switch (opcionUsuario) {
+                    case 1:
+                        cout << "Encuentras valiosos minerales en los asteroides.\n";
+                        cout << "Tu energía aumenta en 15 puntos.\n";
+                        energia += 15;
+                        break;
+                    case 2:
+                        cout << "Exploras el agujero de gusano y encuentras nuevas coordenadas.\n";
+                        cout << "Tu vida aumenta en 10 puntos.\n";
+                        vida += 10;
+                        break;
+                    case 3:
+                        cout << "Descubres un ecosistema sorprendente en el planeta.\n";
+                        cout << "Obtienes conocimiento sobre nuevas formas de vida.\n";
+                        break;
+                    case 4:
+                        cout << "Encuentras tecnología útil en la nave abandonada.\n";
+                        cout << "Tu energía aumenta en 20 puntos.\n";
+                        energia += 20;
+                        break;
+                    case 5:
+                        break; // Regresar al menú principal
+                    default:
+                        cout << "Has tomado una decisión peligrosa...\n";
+                        cout << "¡Tu nave ha sido destruida! Fin del juego.\n";
+                        return 0;
+                }
                 break;
             case 2:
-                cout << "Aterrizas en un mundo alienígena y descubres una reliquia tecnológica.\n";
-                cout << "Tu vida aumenta en 10 puntos y ganas conocimientos alienígenas.\n";
-                vida += 10;
-                break;
-            case 3:
-                cout << "Te enfrentas a una nave enemiga y libras una feroz batalla espacial.\n";
-                cout << "Tu vida disminuye en 30 puntos, pero obtienes valioso botín.\n";
-                vida -= 30;
-                break;
-            case 4:
-                cout << "Te relajas y reabasteces en la estación espacial.\n";
-                cout << "Recuperas 15 puntos de vida y 10 puntos de energía.\n";
-                vida += 15;
-                energia += 10;
-                break;
-            case 5:
-                cout << "¡Tu aventura en el espacio ha terminado por ahora!\n";
+                cout << "¡Gracias por jugar el juego!\n";
                 return 0;
             default:
-                cout << "Opción inválida. Por favor, elige de nuevo.\n";
-                break;
+                cout << "Has elegido una opción inesperada...\n";
+                cout << "¡Tu nave ha sido destruida! Fin del juego.\n";
+                return 0;
         }
 
         if (vida <= 0 || energia <= 0) {
